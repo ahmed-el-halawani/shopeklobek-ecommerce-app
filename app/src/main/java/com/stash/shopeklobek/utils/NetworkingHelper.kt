fb40c8170107ebbf5320d64e44cc8eea19fc6ddb
Ahmed gomaa <agomaa528.ag@gmail.com>
2022-02-21 02:16:19 +0200

    Update NetworkingHelper.kt

--- app/src/main/java/com/stash/shopeklobek/utils/NetworkingHelper.kt
+++ app/src/main/java/com/stash/shopeklobek/utils/NetworkingHelper.kt
@@ -3,24 +3,23 @@ package com.stash.shopeklobek.utils
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities.*
-import java.io.IOException
 
-object NetworkingHelper{
+object NetworkingHelper {
 
-    fun hasInternet(context: Context):Boolean{
+    fun hasInternet(context: Context): Boolean {
         try {
-            val connectivityManager =  context.getSystemService(
+            val connectivityManager = context.getSystemService(
                 Context.CONNECTIVITY_SERVICE
             ) as ConnectivityManager
-            val activeNetwork = connectivityManager.activeNetwork?:return false
-            val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork)?:return false
-            return when{
-                capabilities.hasTransport(TRANSPORT_WIFI)->true
-                capabilities.hasTransport(TRANSPORT_CELLULAR)->true
-                capabilities.hasTransport(TRANSPORT_ETHERNET)->true
-                else->false
+            val activeNetwork = connectivityManager.activeNetwork ?: return false
+            val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+            return when {
+                capabilities.hasTransport(TRANSPORT_WIFI) -> true
+                capabilities.hasTransport(TRANSPORT_CELLULAR) -> true
+                capabilities.hasTransport(TRANSPORT_ETHERNET) -> true
+                else -> false
             }
-        }catch (t:Throwable){
+        } catch (t: Throwable) {
             return false
         }
     }
