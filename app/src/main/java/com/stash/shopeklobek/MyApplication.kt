025df38795b96d7c668576e11fd6416169f3fc23
Ahmed gomaa <agomaa528.ag@gmail.com>
2022-02-25 17:18:33 +0200

    init payment

---
+++ app/src/main/java/com/stash/shopeklobek/MyApplication.kt
@@ -0,0 +1,15 @@
+package com.stash.shopeklobek
+
+import android.app.Application
+import com.paypal.checkout.PayPalCheckout
+import com.paypal.checkout.config.CheckoutConfig
+import com.paypal.checkout.config.Environment
+import com.paypal.checkout.config.SettingsConfig
+import com.paypal.checkout.createorder.CurrencyCode
+import com.paypal.checkout.createorder.UserAction
+
+class MyApplication : Application() {
+
+}
+
+//            returnUrl = "BuildConfig.APPLICATION_ID":
---
+++ app/src/main/java/com/stash/shopeklobek/ui/home/payment_screen/PaymentFragment.kt
@@ -0,0 +1,19 @@
+package com.stash.shopeklobek.ui.home.payment_screen
+
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.payment.paymentsdk.PaymentSdkConfigBuilder
+import com.stash.shopeklobek.databinding.FragmentPaymentBinding
+import com.stash.shopeklobek.databinding.FragmentShippingAddressBinding
+import com.stash.shopeklobek.ui.BaseFragment
+
+class PaymentFragment : BaseFragment<FragmentPaymentBinding>(FragmentPaymentBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
