a9264366dd0d36cd6caf09022776ee7b4dd20a19
Ahmed gomaa <agomaa528.ag@gmail.com>
2022-03-02 01:15:31 +0200

    add basic authentication

--- app/src/main/java/com/stash/shopeklobek/model/api/ApiService.kt
+++ app/src/main/java/com/stash/shopeklobek/model/api/ApiService.kt
@@ -3,7 +3,10 @@ package com.stash.shopeklobek.model.api
 import com.stash.shopeklobek.BuildConfig
 import com.stash.shopeklobek.model.interfaces.ShopifyServices
 import com.stash.shopeklobek.utils.Constants
+import okhttp3.Credentials
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -20,15 +23,25 @@ object ApiService {
         retrofit
     }
 
-     var api = retro.create(ShopifyServices::class.java)
+    var api = retro.create(ShopifyServices::class.java)
+
+    private fun authInterceptor(chain: Interceptor.Chain): Response {
+        val credentials = Credentials.basic(Constants.apiKey, Constants.password);
+        return chain.proceed(
+            chain.request().newBuilder().header("Authorization", credentials)
+                .build()
+        )
+    }
 
     private fun buildAuthClient(): OkHttpClient {
-
         val httpClient = OkHttpClient.Builder()
-        httpClient.addInterceptor( provideLoggingInterceptor())
+        httpClient.addInterceptor(provideLoggingInterceptor())
+            .addInterceptor(::authInterceptor)
         httpClient.connectTimeout(60, TimeUnit.SECONDS)
         httpClient.readTimeout(60, TimeUnit.SECONDS)
-        return httpClient.build()
+        return httpClient
+
+            .build()
     }
 
     private fun provideLoggingInterceptor(): HttpLoggingInterceptor {
