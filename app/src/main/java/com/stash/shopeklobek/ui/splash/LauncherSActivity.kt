204534c1c7233244fd2dc051928a19707b14658e
Ahmed gomaa <agomaa528.ag@gmail.com>
2022-03-04 01:42:22 +0200

    create splash screen

---
+++ app/src/main/java/com/stash/shopeklobek/ui/splash/LauncherSActivity.kt
@@ -0,0 +1,55 @@
+package com.stash.shopeklobek.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.paypal.checkout.PayPalCheckout
+import com.paypal.checkout.config.CheckoutConfig
+import com.paypal.checkout.config.Environment
+import com.paypal.checkout.config.SettingsConfig
+import com.paypal.checkout.createorder.CurrencyCode
+import com.paypal.checkout.createorder.UserAction
+import com.stash.shopeklobek.BuildConfig
+import com.stash.shopeklobek.R
+import com.stash.shopeklobek.ui.MainActivity
+import com.stash.shopeklobek.utils.Constants.PAYPAL_CLIENT_ID
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class LauncherSActivity : AppCompatActivity() {
+    lateinit var splashViewModel: SplashViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher_sactivity)
+
+        splashViewModel = SplashViewModel.create(this)
+        Log.e("splashViewModel", "onCreate: splashViewModel" )
+
+        lifecycleScope.launch {
+                splashViewModel.updateCurrency()
+                splashViewModel.updateLanguage()
+
+                PayPalCheckout.setConfig(
+                    CheckoutConfig(
+                    application = application,
+                    clientId = PAYPAL_CLIENT_ID,
+                    environment = Environment.SANDBOX,
+                    returnUrl = BuildConfig.APPLICATION_ID+"://paypalpay",
+                    currencyCode = CurrencyCode.USD,
+                    userAction = UserAction.PAY_NOW,
+                    settingsConfig = SettingsConfig(
+                        loggingEnabled = true
+                    )
+                )
+                )
+
+            withContext(Dispatchers.Main){
+                startActivity(Intent(this@LauncherSActivity, MainActivity::class.java))
+            }
+        }
+    }
+}
