83bec0113a0e19e4d338a181b12bfee5bd73e25d
Ahmed gomaa <agomaa528.ag@gmail.com>
2022-03-04 18:14:10 +0200

    update and enhancement in order entity

--- app/src/main/java/com/stash/shopeklobek/model/entities/Order.kt
+++ app/src/main/java/com/stash/shopeklobek/model/entities/Order.kt
@@ -2,6 +2,12 @@ package com.stash.shopeklobek.model.entities
 import com.google.gson.annotations.SerializedName
 
 data class Order(
+    @SerializedName( "current_subtotal_price")
+    val finalPrice: String? = "",
+    @SerializedName( "financial_status")
+    val financialStatus: String? = "",
+    @SerializedName( "created_at")
+    val createdAt: String? = "",
 
     @SerializedName( "id")
     val customerId: Long? = 0,
@@ -9,11 +15,9 @@ data class Order(
     @SerializedName( "app_id")
     val orderNumber: Long? = 0,
 
-    @SerializedName( "created_at")
-    val createdAt: String? = "",
 
-    @SerializedName( "current_subtotal_price")
-    val finalPrice: String? = "",
+
+
 
     @SerializedName( "current_total_discounts")
     val totalDiscount: String? = "",
@@ -21,10 +25,13 @@ data class Order(
 //    @SerializedName( "quantity")
 //    val quantity: Long? = 0,
 
-    @SerializedName( "financial_status")
-    val financialStatus: String? = "",
+
 
     @SerializedName( "line_items")
     val items: List<OrderDetails>? = listOf(),
 
-    )
+    ){
+    val dat = createdAt
+    val price = finalPrice
+    val state = financialStatus
+}
