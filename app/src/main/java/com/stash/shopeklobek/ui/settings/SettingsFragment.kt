339dd1d040649c0de94d9f11d1e9641fe969e432
seif mahmoud <Seifoozzz@icloud.com>
2022-03-05 00:46:44 +0200

    enhancment

--- app/src/main/java/com/stash/shopeklobek/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/stash/shopeklobek/ui/settings/SettingsFragment.kt
@@ -2,6 +2,7 @@ package com.stash.shopeklobek.ui.settings
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.stash.shopeklobek.databinding.FragmentSettingsBinding
 import com.stash.shopeklobek.ui.BaseFragment
 
@@ -9,5 +10,14 @@ class SettingsFragment : BaseFragment<FragmentSettingsBinding>(FragmentSettingsB
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.languageGroup.setOnCheckedChangeListener { radioGroup, i ->
+            if (i==binding.btnEArabic.id){
+                Toast.makeText(requireContext(), "arr", Toast.LENGTH_SHORT).show()
+
+            }
+        }
     }
+
+
 }
