c239d6c8186cb4b7cabaf343b65bd0d72fc197e0
Ahmed gomaa <agomaa528.ag@gmail.com>
2022-03-06 03:56:14 +0200

    c

--- app/src/main/java/com/stash/shopeklobek/ui/home/brands/BrandsFragment.kt
+++ app/src/main/java/com/stash/shopeklobek/ui/home/brands/BrandsFragment.kt
@@ -1,9 +1,7 @@
 package com.stash.shopeklobek.ui.home.brands
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.GridLayout
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -13,16 +11,12 @@ import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.interfaces.ItemClickListener
 import com.denzcoskun.imageslider.models.SlideModel
 import com.stash.shopeklobek.databinding.FragmentBrandsBinding
-import com.stash.shopeklobek.databinding.FragmentHomeBinding
-import com.stash.shopeklobek.model.ModelFavorite
 import com.stash.shopeklobek.model.utils.Either
 import com.stash.shopeklobek.model.utils.RepoErrors
 import com.stash.shopeklobek.ui.BaseFragment
-import com.stash.shopeklobek.utils.Constants.TAG
 
 class BrandsFragment : BaseFragment<FragmentBrandsBinding>(FragmentBrandsBinding::inflate) {
 
-    private var imageList = ArrayList<SlideModel>() // Create image list
     private lateinit var brandsAdapter: BrandsAdapter
     private lateinit var recyclerView: RecyclerView
     private lateinit var brandsViewModel: BrandsViewModel
@@ -33,7 +27,7 @@ class BrandsFragment : BaseFragment<FragmentBrandsBinding>(FragmentBrandsBinding
 
         val brandsViewModelFactory = BrandsViewModel.Factory(requireActivity().application)
         brandsViewModel = ViewModelProvider(this, brandsViewModelFactory)[BrandsViewModel::class.java]
-
+        val imageList = ArrayList<SlideModel>()
 
         imageList.add(SlideModel("https://bit.ly/2YoJ77H", "The animal population decreased by 58 percent in 42 years."))
         imageList.add(SlideModel("https://bit.ly/2BteuF2", "Elephants and tigers may become extinct."))
@@ -45,42 +39,38 @@ class BrandsFragment : BaseFragment<FragmentBrandsBinding>(FragmentBrandsBinding
 
         imageSlider.setItemClickListener(object : ItemClickListener {
             override fun onItemSelected(position: Int) {
-                Toast.makeText(requireContext(),"Clicked on Slider",Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Clicked on Slider", Toast.LENGTH_SHORT).show()
             }
         })
         recyclerView = binding.brandRecyclerView
 
         brandsViewModel.getSmartCollection()
         brandsViewModel.brands.observe(viewLifecycleOwner, Observer {
-            when(it){
+            when (it) {
                 is Either.Success -> {
-                    brandsAdapter = BrandsAdapter(it.data.smart_collections!!,requireContext(),this.requireParentFragment())
-                    recyclerView.layoutManager = GridLayoutManager(requireContext(),2,RecyclerView.VERTICAL,false)
+                    brandsAdapter = BrandsAdapter(it.data.smart_collections!!, requireContext(), this.requireParentFragment())
+                    recyclerView.layoutManager = GridLayoutManager(requireContext(), 2, RecyclerView.VERTICAL, false)
                     recyclerView.adapter = brandsAdapter
                 }
-                is Either.Error -> when(it.errorCode){
+                is Either.Error -> when (it.errorCode) {
                     RepoErrors.NoInternetConnection -> Toast.makeText(requireContext(), "No Connection", Toast.LENGTH_SHORT)
                         .show()
                     RepoErrors.ServerError -> Toast.makeText(requireContext(), "Error!", Toast.LENGTH_SHORT).show()
+                    RepoErrors.EmptyBody -> Toast.makeText(requireContext(), "empty body!", Toast.LENGTH_SHORT).show()
                 }
             }
         })
 
         brandsViewModel.loadingLiveData.observe(viewLifecycleOwner, Observer {
-            when(it){
-                true ->{
+            when (it) {
+                true -> {
                     showLoading()
                 }
-                false ->{
+                false -> {
                     hideLoading()
                 }
             }
         })
     }
 
-    override fun onStop() {
-        super.onStop()
-        Log.i(TAG, "onStop: ")
-        imageList.clear()
-    }
 }
