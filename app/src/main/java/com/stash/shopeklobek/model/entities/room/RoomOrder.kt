dbda85d58d2335e79c6bae7776311f1ef69b2750
Ahmed gomaa <agomaa528.ag@gmail.com>
2022-03-12 20:29:35 +0200

    fix order details bug

--- app/src/main/java/com/stash/shopeklobek/model/entities/room/RoomOrder.kt
+++ app/src/main/java/com/stash/shopeklobek/model/entities/room/RoomOrder.kt
@@ -1,5 +1,6 @@
 package com.stash.shopeklobek.model.entities.room
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
@@ -7,13 +8,16 @@ import com.stash.shopeklobek.model.entities.*
 import com.stash.shopeklobek.utils.Constants.CART_TABLE
 import com.stash.shopeklobek.utils.Constants.FAVORITES_TABLE
 import com.stash.shopeklobek.utils.Constants.ORDER_TABLE
+import kotlinx.parcelize.Parcelize
 
 
 @Entity(
     tableName = ORDER_TABLE
 )
+
+@Parcelize
 data class RoomOrder(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val customerEmail:String,
     val order:Order
-)
+) : Parcelable
